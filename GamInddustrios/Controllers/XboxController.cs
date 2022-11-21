871d4b64a43898f8f72f036b7c48f33b56585264
T1maty <timamy666@gmail.com>
2022-11-21 21:07:31 +0300

    Add default cache

--- GamInddustrios/Controllers/XboxController.cs
+++ GamInddustrios/Controllers/XboxController.cs
@@ -2,17 +2,21 @@
 using GamingIndustrios.Service.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 
 namespace GamingIndustrios.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+
     public class XboxController : ControllerBase
     {
+        private readonly IDistributedCache _cache;
         private readonly IXboxServices _xboxServices;
-        public XboxController(IXboxServices xboxServices)
+        public XboxController(IXboxServices xboxServices, IDistributedCache cache)
         {
             _xboxServices = xboxServices;
+            _cache = cache;
         }
         [HttpPost]
         public Xbox AddXbox(Xbox xbox)
