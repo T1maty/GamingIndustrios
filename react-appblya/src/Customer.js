694462c3c0ecfb1799912777a498adfac02b0c30
T1maty <timamy666@gmail.com>
2022-12-06 21:56:28 +0300

    Create Driver

--- react-appblya/src/Customer.js
+++ react-appblya/src/Customer.js
@@ -7,3 +7,34 @@ fetch('https://localhost:7063/api/Customer', {
 }).then(() => {
     console.log('new Customer added');
 })
+
+return (
+    <div className="customer">
+        <h2> Add a Basket</h2>
+        <form onSubmit={handleSubmit}>
+            <label> Customer Title</label>
+            <input
+                type="text"
+                required
+                value={title}
+                onChange={ (e) => setTitle(e.target.value)}
+            />
+            <label>Customer body:</label>
+            <textarea
+                required
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+            ></textarea>
+            <label>Customer Firstname</label>
+            <select
+                value={author}
+                onChange={(e) => setFirstname(e.target.value) }
+            >
+                <option value="Ivan">ivan</option>
+                <option value="Simonyan">simonyan</option>
+            </select>
+            <button>Add To Basket</button>
+        </form>
+    </div>
+
+    )
