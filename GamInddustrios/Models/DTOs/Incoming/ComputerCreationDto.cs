5cf031687de9aff6de161af184b14258bbdf7140
T1maty <timamy666@gmail.com>
2022-12-17 16:45:13 +0300

    add DTOs and Mapping ComputerModels

---
+++ GamInddustrios/Models/DTOs/Incoming/ComputerCreationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GamingIndustrios.Models.DTOs.Incoming
+{
+    public class ComputerCreationDto
+    {
+        public string ComputerName { get; set; }
+        public string Motherboard { get; set; }
+        
+    }
+}
