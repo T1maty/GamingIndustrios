946c9256b56d06a7af9d987769a6c5de26a4adc0
T1maty <timamy666@gmail.com>
2022-12-19 19:46:36 +0300

    Refactoring AuthController

--- GamInddustrios/Controllers/AuthController.cs
+++ GamInddustrios/Controllers/AuthController.cs
@@ -1,6 +1,9 @@
 ﻿using GamingIndustrios.Models.DTOs.Auth;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace GamingIndustrios.Controllers
@@ -10,6 +13,12 @@ namespace GamingIndustrios.Controllers
     public class AuthController : ControllerBase
     {
         public static User user = new User();
+        private readonly IConfiguration _configuration;
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
 
         [HttpPost("register")]
         public async Task<ActionResult<User>> Register(UserRegisterDto request)
@@ -37,9 +46,32 @@ namespace GamingIndustrios.Controllers
             {
                 return BadRequest("Wrong password");
             }
-
-            return Ok("JWT Tokens");
+            string token = CreateToken(user);
+            return Ok(token);
         }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            var token = new JwtSecurityToken(
+                claims:claims,
+                expires:DateTime.Now.AddDays(1),
+                signingCredentials:creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
