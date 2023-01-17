ed504134ef20982a9a82216d8ca78ae334802d1b
T1maty <timamy666@gmail.com>
2023-01-17 12:26:06 +0300

    Refactoring DriverCreationDTO

--- GamInddustrios/Models/DTOs/Incoming/DriverCreationDto.cs
+++ GamInddustrios/Models/DTOs/Incoming/DriverCreationDto.cs
@@ -2,9 +2,9 @@
 {
     public class DriverCreationDto
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public int DriverNumber { get; set; }
-        public int WorldChampionships { get; set; }
+        public string? WorldChampionships { get; set; }
     }
 }
