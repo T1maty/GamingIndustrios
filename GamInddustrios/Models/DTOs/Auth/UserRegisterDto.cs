38b28ba7b0319d063a8c44c0550bc76993e624b3
timofijfedorik <timofijfedorik@gmail.com>
2023-01-25 15:32:06 +0200

    Add GmailAddress

--- GamInddustrios/Models/DTOs/Auth/UserRegisterDto.cs
+++ GamInddustrios/Models/DTOs/Auth/UserRegisterDto.cs
@@ -2,6 +2,7 @@
 {
     public class UserRegisterDto
     {
+        public string GmailAddress { get; set; }
         public string Username { get;set;} = string.Empty;
         public string Password { get;set;} = string.Empty;
     }
