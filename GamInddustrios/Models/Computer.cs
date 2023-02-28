2421157ce8890acad1b99b1f6f31af2a1ad005b0
T1maty <timamy666@gmail.com>
2023-03-01 00:16:58 +0300

    Add MaxLength

--- GamInddustrios/Models/Computer.cs
+++ GamInddustrios/Models/Computer.cs
@@ -1,11 +1,18 @@
-﻿namespace GamingIndustrios.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamingIndustrios.Models
 {
     public class Computer : BaseObject
     {
+        [MaxLength(18)]
         public string? ComputerName { get; set; }
+        [MaxLength(40)]
         public string? MotherBoard { get; set; }
+        [MaxLength(20)]
         public string? Videocards { get; set; }
+        [MaxLength(5)]
         public int Price { get; set; }
+        [MaxLength(10)]
         public string? OS { get; set; }
 
     }
