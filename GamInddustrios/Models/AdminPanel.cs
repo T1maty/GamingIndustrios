cd95e0e7ebe2de5fa092580fb593eb43f8374024
T1maty <timamy666@gmail.com>
2023-03-03 22:56:48 +0300

    Add new in AdminPanel

--- GamInddustrios/Models/AdminPanel.cs
+++ GamInddustrios/Models/AdminPanel.cs
@@ -1,6 +1,15 @@
-﻿namespace GamingIndustrios.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamingIndustrios.Models
 {
     public class AdminPanel
     {
+        [MaxLength(40)]
+        public string GmailAddress { get; set; }
+        [MaxLength(18)]
+        public string Password { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        [MaxLength(10)]
+        public string PhoneNumber { get; set; }
     }
 }
