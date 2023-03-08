edf752a5b54bc4358efbc1f49490fed5bb19b81e
T1maty <timamy666@gmail.com>
2023-03-08 22:57:48 +0300

    Add Max Length

--- GamInddustrios/Models/Auth/User.cs
+++ GamInddustrios/Models/Auth/User.cs
@@ -1,7 +1,10 @@
-﻿namespace GamingIndustrios.Models.Auth
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamingIndustrios.Models.Auth
 {
-    public class User
+    public class User : BaseObject
     {
+        [MaxLength(10)]
         public string Username { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
 
--- GamInddustrios/Models/Auth/UserDto.cs
+++ GamInddustrios/Models/Auth/UserDto.cs
@@ -1,8 +1,14 @@
-﻿namespace GamingIndustrios.Models.Auth
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamingIndustrios.Models.Auth
 {
-    public class UserDto
+    public class UserDto : BaseObject
     {
+        [MaxLength(20)]
+        public string Gmail { get; set; } = string.Empty;
+        [MaxLength(15)]
         public string Username { get; set; } = string.Empty;
+        [MaxLength(18)]
         public string Password { get; set; } = string.Empty;
     }
 }
