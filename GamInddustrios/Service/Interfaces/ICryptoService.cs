80098402d611adf64dd5daff887d5d8d5d2feb76
T1maty <timamy666@gmail.com>
2023-03-13 16:54:06 +0300

    Add Service Crypto

---
+++ GamInddustrios/Service/Interfaces/ICryptoService.cs
@@ -0,0 +1,9 @@
+﻿using GamingIndustrios.Models;
+
+namespace GamingIndustrios.Service.Interfaces
+{
+    public interface ICryptoService
+    {
+        public TransferCrypto AddTransferCrypto(TransferCrypto transferCrypto);
+    }
+}
