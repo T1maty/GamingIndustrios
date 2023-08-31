ccc0c558c41591ae9de8b518ccd112e46f0c61f8
T1maty <timamy666@gmail.com>
2023-08-31 18:43:19 +0300

    Add Encryption Middleware

---
+++ GamInddustrios/Middleware/Encrypt/EncryptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿namespace GamingIndustrios.Middleware.Encrypt
+{
+    public class EncryptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public EncryptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Response.Body = EncryptStream(httpContext.Response.Body);
+            httpContext.Request.Body = DecryptStream(httpContext.Request.Body);
+            if (httpContext.Request.QueryString.HasValue)
+            {
+                string decryptedString = DecryptString(httpContext.Request.QueryString.Value.Substring(1));
+                httpContext.Request.QueryString = new QueryString($"?{decryptedString}");
+            }
+            await _next(httpContext);
+            await httpContext.Request.Body.DisposeAsync();
+            await httpContext.Response.Body.DisposeAsync();
+        }
+
+        private Stream EncryptStream(Stream body)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Stream DecryptStream(Stream body)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string DecryptString(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
